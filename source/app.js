4c851d317c5b63e44415cd652be3c8c06b4e5294
Сушко Александр Сергеевич <sushko@skbkontur.ru>
2017-06-06 16:02:42 +0500

    Добавит компонент вывода дел списком и их редактирования

--- source/app.js
+++ source/app.js
@@ -6,30 +6,31 @@ class Input extends React.Component {
     /** Конструктор */
     constructor(props) {
         super(props);
-   
+
         // Данные по умолчанию
         this.state = {
-            value: ''
+            value: props.value || '',
+            index: props.index || null
         };
-   
+
         // Байндим методы на this, чтобы не было проблем с контекстом
         this.submitItem = this.submitItem.bind(this);
         this.inputItem = this.inputItem.bind(this);
     }
-   
+
     /**
      * Отправляет введённое значение в список
      */
     submitItem(event) {
         event.preventDefault();
         // Отправляем занчение в коллбек от компонента-родителя
-        this.props.onSubmitItem(this.state.value);
+        this.props.onSubmitItem(this.state.value, this.state.index);
         // Удаляем введённое значение после отправки
         this.setState({
             value: ''
         })
     }
-   
+
     /**
      * Записывает введённое значение в стейт
      */
@@ -39,32 +40,83 @@ class Input extends React.Component {
             value: event.target.value
         });
     }
-    
+
     /** Рендер */
     render() {
         return (
             <form onSubmit={this.submitItem}>
-                <input type="text" placeholder="Например, купить хлеба" value={this.state.value} onChange={this.inputItem} />
+                <input type="text" value={this.state.value} onChange={this.inputItem} />
             </form>
         );
     }
 }
 
+/** Список дел */
+function List(props) {
+    // Создаем новый массив и разворачиваем его, чтобы новые пункты добавлялись сверху
+    const reverseItems = props.items.slice().reverse();
+    // Записываем длинну исходного массива, чтобы после проставить верные ключи
+    const length = props.items.length;
+    // Собираем пункты списка из нового массива
+    const items = reverseItems.map((item, index, arr) => {
+        console.log(length, index);
+        // Вычисляем исходный ключ
+        const key = length - index;
+        return (
+            <li key={key}>
+                <span>{item}</span>
+                <Input index={key} value={item} onSubmitItem={props.onEditItem} />
+            </li>
+        )
+    });
+
+    // Возвращаем готовый список
+    return (
+        <ul>{items}</ul>
+    )
+}
+
 class ToDo extends React.Component {
+    /** Конструктор */
     constructor(props) {
         super(props);
-   
+
+        // Данные по умолчанию
+        this.state = {
+            items: []
+        };
+
         // Байндим методы на this, чтобы не было проблем с контекстом
         this.recordItem = this.recordItem.bind(this);
+        this.editItem = this.editItem.bind(this);
     }
-   
+
     /**
      * Записываем значение в базу
      * @param {string} value
      */
     recordItem(value) {
-        console.log('"' + value + '" recorded');
-        // ToDo: разобраться, в каком виде удобнее писать в стейт
+        this.setState((prevState) => ({
+            // ToDo: разобраться, что за именяемые данные
+            items: prevState.items.concat(value)
+        }));
+    }
+
+    /**
+     * Изменяем значение в базе значение в базу
+     * @param {number} key
+     * @param {string} value
+     */
+    editItem(value, key) {
+        this.setState((prevState) => {
+            const items = prevState.items;
+            // ToDo: разобраться, почему key раздаётся с единицы
+            const index = key - 1;
+            items[index] = value;
+            return {
+                items: items
+            }
+        });
     }
 
     render() {
@@ -72,6 +124,7 @@ class ToDo extends React.Component {
             <div>
                 <h1>ToDo</h1>
                 <Input onSubmitItem={this.recordItem} />
+                <List items={this.state.items} onEditItem={this.editItem} />
             </div>
         );
     }
