a264b9d6e9c2c63ef6038d0728a4296a5e874e3c
Сушко Александр Сергеевич <sushko@skbkontur.ru>
2017-06-14 12:01:29 +0500

    Добавит отдельный конфиг Вебпака для Сториес

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,8 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-    entry: './src/js/App.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
-    },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.(js|jsx)?$/,
                 loader: 'babel-loader',
@@ -34,20 +28,7 @@ const config = {
             'node_modules',
             'web_modules'
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            inject: 'body',
-            minify: {
-                collapseWhitespace: true
-            }
-        })
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        port: 9000
     }
-};
+}
 
 module.exports = config;
