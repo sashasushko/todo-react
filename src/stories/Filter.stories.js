ba65f49058817105af43801df3c38b35ff89c81c
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-08-01 11:14:26 +0500

    Поправит стори

--- src/stories/Filter.stories.js
+++ src/stories/Filter.stories.js
@@ -5,18 +5,10 @@ import { action } from "@storybook/addon-actions";
 
 import Filter from "./../components/Filter/Filter";
 
-storiesOf("Filter", module)
-  .add("default", () =>
-    <Filter
-      filter="all"
-      onChange={action("onChange")}
-      onRemove={action("onRemove")}
-    />
-  )
-  .add("with presetted filter", () =>
-    <Filter
-      filter="all"
-      onChange={action("onChange")}
-      onRemove={action("onRemove")}
-    />
-  );
+storiesOf("Filter", module).add("default", () =>
+  <Filter
+    filter="all"
+    onChange={action("onChange")}
+    onRemove={action("onRemove")}
+  />
+);
