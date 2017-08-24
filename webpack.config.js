8692ac7ba986893e0104b99def0f33f87ae06b3e
sashasushko <sushko@outlook.com>
2017-08-24 16:03:40 +0500

    Добавит преакт

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 const PROD = process.env.NODE_ENV === "production";
 
@@ -54,18 +55,34 @@ const config = {
     ]
   },
   resolve: {
-    modules: ["node_modules", "web_modules"]
+    modules: ["node_modules", "web_modules"],
+    alias: {
+      react: "preact-compat",
+      "react-dom": "preact-compat",
+      "preact-compat": "preact-compat/dist/preact-compat"
+    }
   },
-  plugins: [
-    new ExtractTextPlugin("app.css"),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      inject: "body",
-      minify: {
-        collapseWhitespace: true
-      }
-    })
-  ]
+  plugins: PROD
+    ? [
+        new UglifyJSPlugin({ extractComments: { banner: false } }),
+        new ExtractTextPlugin("app.css"),
+        new HtmlWebpackPlugin({
+          template: "./src/index.html",
+          inject: "body",
+          minify: {
+            collapseWhitespace: true
+          }
+        })
+      ]
+    : [
+        new HtmlWebpackPlugin({
+          template: "./src/index.html",
+          inject: "body",
+          minify: {
+            collapseWhitespace: true
+          }
+        })
+      ]
 };
 
 module.exports = config;
